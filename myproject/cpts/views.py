4606e277c66a6dddc75938d3b137afe328e1e221
terman37 <terman37@yahoo.fr>
2020-01-13 22:24:33 +0100

    Update views.py

--- myproject/cpts/views.py
+++ myproject/cpts/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def summary(request):
-    return render(request,'cpts/test.html',{})
+    return render(request,'cpts/summary.html',{})
