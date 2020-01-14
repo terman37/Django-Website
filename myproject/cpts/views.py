563a7bddba99b5cde16a4df8a4e6681c9f28321c
terman37 <terman37@yahoo.fr>
2020-01-14 11:14:46 +0100

    Update views.py

--- myproject/cpts/views.py
+++ myproject/cpts/views.py
@@ -5,5 +5,6 @@ from .models import Accounts
 
 def summary(request):
     pagetitle = "Summary"
-    accounts = Accounts.objects.all()
+    types = Accounts.objects.distinct(t_type)
+    accounts = Accounts.objects.exclude(d_inactive='NULL')
     return render(request, 'cpts/summary.html', {'title': pagetitle, 'mydatas': accounts})
