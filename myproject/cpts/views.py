8416cbf670fb5cc76b44cb0f515ec8036e593094
terman37 <terman37@yahoo.fr>
2020-01-14 16:57:34 +0100

    Update views.py

--- myproject/cpts/views.py
+++ myproject/cpts/views.py
@@ -10,6 +10,6 @@ def summary(request):
     # accounts = []
     # for cpt_type in types:
     #     accounts.append(Accounts.objects.filter(t_type=cpt_type))
-    accounts = Accounts.objects.order_by('t_type','t_name').exclude(d_inactive__gt=datetime.date(2010, 1, 1))
+    accounts = Accounts.objects.filter(d_inactive__isnull=True).order_by('t_type','t_name')
     # return render(request, 'cpts/summary.html', {'title': pagetitle, 'mytypes': types, 'mydatas': accounts})
     return render(request, 'cpts/summary.html', {'title': pagetitle, 'mydatas': accounts})
