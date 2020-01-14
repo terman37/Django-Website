2b6a868f0a53698d8db8ca8c1dc8afaa1d5a7cbb
terman37 <terman37@yahoo.fr>
2020-01-14 11:37:34 +0100

    summary v0.08

--- myproject/cpts/views.py
+++ myproject/cpts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Accounts
 
+
 # Create your views here.
 
 def summary(request):
     pagetitle = "Summary"
-    types = Accounts.objects.distinct(t_type)
-    accounts = Accounts.objects.exclude(d_inactive='NULL')
-    return render(request, 'cpts/summary.html', {'title': pagetitle, 'mydatas': accounts})
+    types = Accounts.objects.values_list('t_type', flat=True).distinct()
+    accounts = []
+    for cpt_type in types:
+        accounts.append(Accounts.objects.filter(t_type=cpt_type))
+    # accounts = Accounts.objects.exclude(d_inactive='NULL')
+    return render(request, 'cpts/summary.html', {'title': pagetitle, 'mytypes': types, 'mydatas': accounts})
