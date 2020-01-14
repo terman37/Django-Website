dbfe4bed8581a18be8f5e6eb12b3c30db79b15ea
terman37 <terman37@yahoo.fr>
2020-01-14 16:21:55 +0100

    Update views.py

--- myproject/cpts/views.py
+++ myproject/cpts/views.py
@@ -10,6 +10,6 @@ def summary(request):
     # accounts = []
     # for cpt_type in types:
     #     accounts.append(Accounts.objects.filter(t_type=cpt_type))
-    accounts = Accounts.objects.all()
+    accounts = Accounts.objects.order_by('t_type').all()
     # return render(request, 'cpts/summary.html', {'title': pagetitle, 'mytypes': types, 'mydatas': accounts})
     return render(request, 'cpts/summary.html', {'title': pagetitle, 'mydatas': accounts})
