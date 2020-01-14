be6186092d5c740536f979c0650195c0a03c2957
terman37 <terman37@yahoo.fr>
2020-01-14 12:32:35 +0100

    summary v0.09

--- myproject/cpts/views.py
+++ myproject/cpts/views.py
@@ -7,8 +7,9 @@ from .models import Accounts
 def summary(request):
     pagetitle = "Summary"
     types = Accounts.objects.values_list('t_type', flat=True).distinct()
-    accounts = []
-    for cpt_type in types:
-        accounts.append(Accounts.objects.filter(t_type=cpt_type))
-    # accounts = Accounts.objects.exclude(d_inactive='NULL')
-    return render(request, 'cpts/summary.html', {'title': pagetitle, 'mytypes': types, 'mydatas': accounts})
+    # accounts = []
+    # for cpt_type in types:
+    #     accounts.append(Accounts.objects.filter(t_type=cpt_type))
+    accounts = Accounts.objects.all()
+    # return render(request, 'cpts/summary.html', {'title': pagetitle, 'mytypes': types, 'mydatas': accounts})
+    return render(request, 'cpts/summary.html', {'title': pagetitle, 'mydatas': accounts})
