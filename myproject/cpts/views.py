b6b9c1b3c3c550615f02a1fe64ddfa150123ce78
terman37 <terman37@yahoo.fr>
2020-01-15 14:00:16 +0100

    v0.21

--- myproject/cpts/views.py
+++ myproject/cpts/views.py
@@ -1,14 +1,28 @@
-from django.contrib.auth.decorators import login_required
+# Basics
 from django.shortcuts import render
 from .models import Accounts
+# Decorators
+from django.contrib.auth.decorators import login_required
+# import
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
 
 @login_required
 def summary(request):
     pagetitle = "Summary"
-    accounts = Accounts.objects.filter(d_inactive__isnull=True).order_by('t_type','t_name')
+    accounts = Accounts.objects.filter(d_inactive__isnull=True).order_by('t_type', 't_name')
     return render(request, 'cpts/summary.html', {'title': pagetitle, 'mydatas': accounts})
+
 
 @login_required
 def importofx(request):
     pagetitle = "Import Ofx"
+
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'cpts/importofx.html', {'title': pagetitle, 'uploaded_file_url': uploaded_file_url})
     return render(request, 'cpts/importofx.html', {'title': pagetitle})
