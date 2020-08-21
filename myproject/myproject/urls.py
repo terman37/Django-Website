ea5d4247ea38bf669bb481480742f4821e8ccbb4
Anthony Jourdan <terman37@yahoo.fr>
2020-08-21 19:00:27 +0200

    update

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('cpts.urls')),
+    path('', include('cpts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
